ddd62b46cdda96b575718a92efa330dfa67b4e86
raziman <raziman.terra@gmail.com>
2021-06-25 08:02:23 +0800

    add arguments to battle command

--- src/commands/battle.ts
+++ src/commands/battle.ts
@@ -9,13 +9,16 @@ export async function battle(msg: Message, args: string[]) {
 
   const member = msg.guild?.members.cache.get(msg.author.id);
   const isRegistered = await hasUser(msg.author.id);
+  const level = args[0];
 
   if (!member)
     return msg.channel.send("Member does not exist");
   else if (!isRegistered)
     return msg.channel.send("User has not registered to any challenge");
+  else if (level && !parseInt(level))
+    return msg.channel.send("Invalid level");
 
   const player = await Player.getPlayer(member);
-  const challenger = await Challenger.getChallenger(player.level);
+  const challenger = await Challenger.getChallenger(parseInt(level) || player.level);
   await battleSimulator(msg, player, challenger);
 }
