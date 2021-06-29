d99f818b377041c27ba0fb67ddc1f8a8899b7a73
raziman <raziman.terra@gmail.com>
2021-06-29 19:07:53 +0800

    remove user checking because Player class will create profile by default if not exists

--- src/commands/battle.ts
+++ src/commands/battle.ts
@@ -1,5 +1,4 @@
 import { Collection, Message, MessageReaction, User } from "discord.js";
-import hasUser from "../db/hasUser";
 import { battle as battleSimulator } from "../internals/battle";
 import { Player } from "../internals/player";
 import { Challenger } from "../internals/challenger";
@@ -16,12 +15,9 @@ export async function battle(msg: Message, _: string[]) {
 
   try {
     const member = msg.guild?.members.cache.get(msg.author.id);
-    const isRegistered = await hasUser(msg.author.id);
 
     if (!member)
       return msg.channel.send("Member does not exist");
-    else if (!isRegistered)
-      return msg.channel.send("User has not registered to any challenge");
 
     const player = await Player.getPlayer(member);
 
