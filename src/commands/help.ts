17cb6948ad1d114ce274b9d75b7d30fd49438814
raziman <raziman.terra@gmail.com>
2021-07-10 13:10:31 +0800

    update help message

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -1,24 +1,21 @@
+import { stripIndents } from "common-tags";
 import { Message } from "discord.js";
 
 
 export default function(msg: Message, args: string[]) {
 
-  const message = `
-  \`\`\`yaml
-  Command Manual
+  const message = stripIndents`
+  **$profile**
+  This will show your profile with all your ranking, level, stats, buffs and
+  energy. This is your main overview of your character.
 
-  [] - optional
-  <> - required
-
-  $rank [count]
-  Reset rank leaderboard. Count is the number of users to be shown on the rank
-  board. Leaving it out causes the board to show all players.
-
-  $award <userId> <amount> <reason>
-  Add xp to user. To deduct, use negative amount.
-
-  $profile [userId]
-  Show user profile. User id can be given to show other user profile.\`\`\``
+  **$battle**
+  This will allow you to battle Ares Challengers from level 1 to level 50. You
+  can only challenge 1 level higher than your maximum defeated level. Winning
+  against challengers award coins. You can only battle if you have energy, it
+  replenishes automatically every 8 hours to a max of 5. Check your current
+  energy with $profile!
+  `
 
 
   msg.channel.send(message);
