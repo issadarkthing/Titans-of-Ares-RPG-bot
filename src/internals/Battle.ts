68c837ec09750e58bbc1446f80b463d22f0bb094
raziman <raziman.terra@gmail.com>
2021-07-12 10:35:37 +0800

    fix battle error

--- src/internals/Battle.ts
+++ src/internals/Battle.ts
@@ -19,12 +19,19 @@ export class Battle {
 
   private round = 0;
   private playerRound = 0;
+  private battleMsg?: Message;
+  private battleEmbed?: MessageEmbed;
+  private playerMaxHP: number;
+  private challengerMaxHP: number;
   
   constructor(
     private msg: Message,
     private player: Player,
     private challenger: Challenger,
-  ) {}
+  ) {
+    this.playerMaxHP = this.player.hp;
+    this.challengerMaxHP = this.challenger.hp;
+  }
 
   private isEven(num: number) {
     return num % 2 === 0;
@@ -44,12 +51,92 @@ export class Battle {
       .join("");
   }
 
+  async attack(p1: Fighter, p2: Fighter) {
+
+    const isCrit = p1.isCriticalHit();
+    const attackRate = isCrit ? CRIT_RATE * p1.strength : p1.strength;
+    const damageReduction = p1.getArmorReduction(attackRate);
+    const damageDone = (attackRate - damageReduction);
+    p2.hp -= damageDone;
+
+    const critText = isCrit ? " (x2 critical hit)" : "";
+
+    this.battleEmbed = new MessageEmbed()
+      .setColor(RED)
+      .setThumbnail(p1.imageUrl)
+      .addField("Attacking Player", p1.name)
+      .addField("Attack Rate", `\`${attackRate}${critText}\``, true)
+      .addField("Damage Reduction", `\`${damageReduction}\``, true)
+      .addField("Damage Done", `\`${damageDone}\``, true)
+      .addField("Round", this.round + 1, true);
+
+    const createCritEmbed = async (url: string) => {
+      const critEmbed = new MessageEmbed()
+        .setTitle(`${p1.name} Critical Attack`)
+        .setColor(RED)
+        .setImage(url);
+
+      await this.battleMsg?.edit(critEmbed);
+    };
+
+    if (p1 instanceof Player) {
+      this.playerRound++;
+      const p1MaxHP = numberFormat(this.playerMaxHP);
+      const p1HealthBar = this.progressBar(p1.hp, this.playerMaxHP);
+      const p1RemainingHp = p1.hp >= 0 ? numberFormat(p1.hp) : 0;
+
+      const p2MaxHP = numberFormat(this.challengerMaxHP);
+      const p2HealthBar = this.progressBar(p2.hp, this.challengerMaxHP);
+      const p2RemainingHp = p2.hp >= 0 ? numberFormat(p2.hp) : 0;
+
+
+      this.battleEmbed.addField(
+        `${p1.name}'s remaining HP`,
+        `${p1HealthBar} \`${p1RemainingHp}/${p1MaxHP}\``
+      );
+      this.battleEmbed.addField(
+        `${p2.name}'s remaining HP`,
+        `${p2HealthBar} \`${p2RemainingHp}/${p2MaxHP}\``
+      );
+
+    } else {
+      const p1MaxHP = numberFormat(this.challengerMaxHP);
+      const p1HealthBar = this.progressBar(p1.hp, this.challengerMaxHP);
+      const p1RemainingHp = p1.hp >= 0 ? numberFormat(p1.hp) : 0;
+
+      const p2MaxHP = numberFormat(this.playerMaxHP);
+      const p2HealthBar = this.progressBar(p2.hp, this.playerMaxHP);
+      const p2RemainingHp = p2.hp >= 0 ? numberFormat(p2.hp) : 0;
+
+      this.battleEmbed.addField(
+        `${p2.name}'s remaining HP`,
+        `${p2HealthBar} \`${p2RemainingHp}/${p2MaxHP}\``
+      );
+      this.battleEmbed.addField(
+        `${p1.name}'s remaining HP`,
+        `${p1HealthBar} \`${p1RemainingHp}/${p1MaxHP}\``
+      );
+    }
+
+    if (isCrit) {
+      if (p1.name === this.player.name) {
+        await createCritEmbed(PLAYER_CRIT_GIF);
+      } else {
+        await createCritEmbed(CHALLENGER_CRIT_GIF);
+      }
+
+      await sleep(4000);
+    }
+
+    await this.battleMsg?.edit(this.battleEmbed);
+  }
+
   async run() {
     let done = false;
-    let embed = new MessageEmbed();
-    const message = await this.msg.channel.send("Battle start");
-    const playerMaxHP = this.player.hp;
-    const challengerMaxHP = this.challenger.hp;
+    this.battleEmbed = new MessageEmbed();
+    this.battleMsg = await this.msg.channel.send("Battle start");
+    this.playerMaxHP = this.player.hp;
+    this.challengerMaxHP = this.challenger.hp;
 
     // This determines whichever moves first. If the player has higher speed, it
     // returns 0, i.e, player moves first by default. Otherwise, it returns 1
@@ -62,89 +149,11 @@ export class Battle {
       ? 0
       : 1;
 
-    const attack = async (p1: Fighter, p2: Fighter) => {
-      const isCrit = p1.isCriticalHit();
-      const attackRate = isCrit ? CRIT_RATE * p1.strength : p1.strength;
-      const damageReduction = p1.getArmorReduction(attackRate);
-      const damageDone = (attackRate - damageReduction);
-      p2.hp -= damageDone;
-
-      const critText = isCrit ? " (x2 critical hit)" : "";
-
-      embed = new MessageEmbed()
-        .setColor(RED)
-        .setThumbnail(p1.imageUrl)
-        .addField("Attacking Player", p1.name)
-        .addField("Attack Rate", `\`${attackRate}${critText}\``, true)
-        .addField("Damage Reduction", `\`${damageReduction}\``, true)
-        .addField("Damage Done", `\`${damageDone}\``, true)
-        .addField("Round", this.round + 1, true);
-
-      const createCritEmbed = async (url: string) => {
-        const critEmbed = new MessageEmbed()
-          .setTitle(`${p1.name} Critical Attack`)
-          .setColor(RED)
-          .setImage(url);
-
-        await message.edit(critEmbed);
-      };
-
-      if (p1.name === this.player.name) {
-        const p1MaxHP = numberFormat(playerMaxHP);
-        const p1HealthBar = this.progressBar(p1.hp, playerMaxHP);
-        const p1RemainingHp = p1.hp >= 0 ? numberFormat(p1.hp) : 0;
-
-        const p2MaxHP = numberFormat(challengerMaxHP);
-        const p2HealthBar = this.progressBar(p2.hp, challengerMaxHP);
-        const p2RemainingHp = p2.hp >= 0 ? numberFormat(p2.hp) : 0;
-
-
-        embed.addField(
-          `${p1.name}'s remaining HP`,
-          `${p1HealthBar} \`${p1RemainingHp}/${p1MaxHP}\``
-        );
-        embed.addField(
-          `${p2.name}'s remaining HP`,
-          `${p2HealthBar} \`${p2RemainingHp}/${p2MaxHP}\``
-        );
-
-      } else {
-        const p1MaxHP = numberFormat(challengerMaxHP);
-        const p1HealthBar = this.progressBar(p1.hp, challengerMaxHP);
-        const p1RemainingHp = p1.hp >= 0 ? numberFormat(p1.hp) : 0;
-
-        const p2MaxHP = numberFormat(playerMaxHP);
-        const p2HealthBar = this.progressBar(p2.hp, playerMaxHP);
-        const p2RemainingHp = p2.hp >= 0 ? numberFormat(p2.hp) : 0;
-
-        embed.addField(
-          `${p2.name}'s remaining HP`,
-          `${p2HealthBar} \`${p2RemainingHp}/${p2MaxHP}\``
-        );
-        embed.addField(
-          `${p1.name}'s remaining HP`,
-          `${p1HealthBar} \`${p1RemainingHp}/${p1MaxHP}\``
-        );
-      }
-
-      if (isCrit) {
-        if (p1.name === this.player.name) {
-          await createCritEmbed(PLAYER_CRIT_GIF);
-        } else {
-          await createCritEmbed(CHALLENGER_CRIT_GIF);
-        }
-
-        await sleep(4000);
-      }
-
-      await message.edit(embed);
-    };
-
     while (!done) {
       if (this.isEven(this.round + moveFirst)) {
-        await attack(this.player, this.challenger);
+        await this.attack(this.player, this.challenger);
       } else {
-        await attack(this.challenger, this.player);
+        await this.attack(this.challenger, this.player);
       }
 
       if (this.player.hp <= 0 || this.challenger.hp <= 0) break;
@@ -162,8 +171,8 @@ export class Battle {
 
     if (isWon) {
       const loot = this.challenger.loot;
-      embed.setColor(GOLD);
-      await message.edit(embed);
+      this.battleEmbed.setColor(GOLD);
+      await this.battleMsg.edit(this.battleEmbed);
       await this.player.addCoin(loot);
       await setMaxChallenger(this.player.id, this.challenger.level);
       this.player.challengerMaxLevel = this.challenger.level;
