fba2223e41821be476ac60ae0f1a5c11e4c6ce60
raziman <raziman.terra@gmail.com>
2021-07-22 10:33:30 +0800

    add scroll

---
+++ src/internals/Scroll.ts
@@ -0,0 +1,28 @@
+import { MessageEmbed } from "discord.js";
+import { removeInventory } from "../db/inventory";
+import { Item } from "./Item";
+import { Player } from "./Player";
+import { BROWN } from "./utils";
+
+
+export class Scroll extends Item {
+  id = "scroll";
+  name = "Upgrade Scroll";
+  price = 25;
+
+  show(count: number) {
+    
+    const embed = new MessageEmbed()
+      .setColor(BROWN)
+      .setTitle("Upgrade Scroll")
+      .setDescription("Scroll is used to upgrade gears")
+      .addField("Price", this.price, true)
+      .addField("Count", count, true)
+
+    return embed;
+  }
+
+  use(player: Player) {
+    return removeInventory(player.id, this.id);
+  }
+}
