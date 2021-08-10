7f37aed390f22a26c941577f3a59468f270077db
raziman <raziman.terra@gmail.com>
2021-08-10 09:05:47 +0800

    add blocking option to command

--- src/internals/Command.ts
+++ src/internals/Command.ts
@@ -6,6 +6,9 @@ import path from "path";
 export default abstract class Command {
   abstract name: string;
   aliases: string[] = [];
+  /** blocks command if there's already an instance of it running under the same
+   * player*/
+  block = false;
 
   abstract exec(msg: Message, args: string[]): unknown | Promise<unknown>;
 }
@@ -14,6 +17,7 @@ const readdir = util.promisify(fs.readdir);
 
 export class CommandManager {
   private commands = new Map<string, Command>();
+  private blockList = new Set<string>();
 
   async registerCommands(dir: string) {
     const files = await readdir(dir);
@@ -26,12 +30,24 @@ export class CommandManager {
     }
   }
 
-  handleMessage(cmd: string, msg: Message, args: string[]) {
-    
+  async handleMessage(cmd: string, msg: Message, args: string[]) {
     const command = this.commands.get(cmd);
-
     if (!command)
       return;
+
+    if (command.block) {
+      const id = `${command.name}_${msg.author.id}`;
+      if (this.blockList.has(id)) {
+        msg.channel.send(
+          `There's already an instance of ${command.name} command running`
+        );
+      } else {
+        this.blockList.add(id);
+        await command.exec(msg, args);
+        this.blockList.delete(id);
+      }
+      return;
+    }
 
     command.exec(msg, args);
   }
