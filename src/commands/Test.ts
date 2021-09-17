e1c34be9542d52fbbe5ca95a176be4a7162f5ce3
raziman <raziman.terra@gmail.com>
2021-09-17 16:08:02 +0800

    add more option to helper command

--- src/commands/Test.ts
+++ src/commands/Test.ts
@@ -1,5 +1,6 @@
 import { Message } from "discord.js";
 import { addGem, setMiningPickReward } from "../db/gem";
+import { addXP } from "../db/xp";
 import Command from "../internals/Command";
 import { Common } from "../internals/Mining";
 import { MiningPickReward } from "../internals/MiningPickReward";
@@ -13,7 +14,7 @@ export default class extends Command {
 
   async exec(msg: Message, args: string[]) {
 
-    const [arg1] = args;
+    const [arg1, arg2] = args;
 
     // reset MiningPickReward column
     if (arg1 === "db" && msg.author.id === client.devID) {
@@ -27,14 +28,22 @@ export default class extends Command {
     }
 
     if (!client.isDev) return;
-
+    
     const player = await Player.getPlayer(msg.member!);
 
-    for (let i = 0; i < 10; i++) {
-      const gem = Common.random();
-      await addGem(player.id, gem.id);
-      await msg.channel.send(`You got ${gem.name}!`);
-      await msg.channel.send(gem.show(-1));
+    if (arg1 === "xp") {
+      addXP(player.id, parseInt(arg2));
+
+    } else if (arg1 === "gem") {
+
+      for (let i = 0; i < 10; i++) {
+        const gem = Common.random();
+        await addGem(player.id, gem.id);
+        await msg.channel.send(`You got ${gem.name}!`);
+        await msg.channel.send(gem.show(-1));
+      }
     }
+
+
   }
 }
